db7dffa83edfc2005c1ec8d0c70ace4c9aca8ba7
winnie <asgh03226@gmail.com>
2021-02-10 22:00:02 +0800

    Installed: -TailwindCss -postcss -purgecss

--- app/frontends/packs/application.js
+++ app/frontends/packs/application.js
@@ -7,7 +7,16 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "scripts" 
+
+//Tailwind CSS
+import "stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+//images
+const images = require.context('../images', true)
+
+
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-module.exports = {
+let environment = {
   plugins: [
     require('postcss-import'),
+    require('tailwindcss'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
@@ -9,4 +10,23 @@ module.exports = {
       stage: 3
     })
   ]
+};
+
+if (process.env.RAILS_ENV === 'production') {
+  environment.plugins.push(
+    require('purgecss')({
+      content: [
+        './app/**/.html.erb',
+        './app/helpers/**/*.rb',
+        './app/frontend/**/*.js',
+        './app/frontend/**/*.vue',
+        './app/frontend/**/*.jsx',
+        './app/frontend/**/*.css',
+        './app/frontend/**/*.scss'
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
 }
+
+module.exports = environment;
